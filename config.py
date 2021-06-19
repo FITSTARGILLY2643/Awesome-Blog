d216e60e9c428629cfaebdd920cea151fb41b68e
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-19 17:42:24 +0300

    Create the general configuration class

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
+    
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
