cd9bd1f78c792bd0c793bfaaa483cd8602111d5b
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-19 19:38:19 +0300

    Created the blogs and the user classes

---
+++ app/models.py
@@ -0,0 +1,70 @@
+from . import db
+from datetime import datetime
+from . import login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Blogs(db.Model):
+
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    blog = db.Column(db.String(255))
+    date = db.Column(db.DateTime(250), default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id",ondelete='CASCADE'), nullable=False)
+    comments = db.relationship('Comments', backref='title', lazy='dynamic')
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteblog(self):
+        db.session.delete(self)
+        db.session.commit() 
+
+    @classmethod
+    def get_blogs(cls):
+        blog = Blogs.query.all()
+        return blog
+
+
+    def __repr__(self):
+        return f"Blogs {self.blog}','{self.date}')"     
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    author = db.Column(db.String(255),index=True)
+    email = db.Column(db.String(255),unique=True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blog = db.relationship('Blogs', backref='author',passive_deletes=True, lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+    comment = db.relationship('Comments',backref = 'author',passive_deletes=True,lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+
+    def __repr__(self):
+        return f'User {self.author}'
